1c76b0dffdc1cf40a50a650d072ada07cf5fc214
Sam White <samwhiteiv@gmail.com>
2020-05-08 14:10:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+### Links
+
+- https://github.com/samwhiteiv/lunchstudio-webflow-react.git
+- https://webflow.com/blog/react-components-in-webflow
+- https://webflow.com/website/Webflow-React
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div>This is a React component inside of Webflow!</div>
+     )
+   }
+ }
+
+ReactDOM.render(
+  React.createElement(App, {}, null),
+  document.getElementById('react-target')
+);
